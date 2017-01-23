a6a4e5210efbdd8c0a60e69057410396be99a91c
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-01-23 14:08:03 +0200

    refactored ecommerce-app by adding a show REST

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,9 @@ class ProductsController < ApplicationController
     @all_products = Product.all 
     render "index.html"
   end 
+
+  def show 
+  @product = Product.find_by(id: params[:id]) 
+  render "show.html.erb"
+  end
 end

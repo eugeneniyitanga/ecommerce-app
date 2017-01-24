d43e25821702f04c51a880373c4135ff51278fa9
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-01-24 16:10:55 +0200

    added edit, update and estroy REST actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,24 @@ class ProductsController < ApplicationController
     product = Product.new({name: name, brand: brand, price: price})
     product.save 
   end 
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.brand = params[:brand]
+    @product.price = params[:price]
+    @product.save
+  end
+
+  def destroy 
+     @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.brand = params[:brand]
+    @product.price = params[:price]
+    @product.save
+   end 
 end
